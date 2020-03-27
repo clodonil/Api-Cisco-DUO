abd474a5242a29d7ee36efbfcab9abfbdded5882
clodonil <clodonil@nisled.org>
2020-03-26 23:32:35 -0300

    tratamento o erro

--- api.py
+++ api.py
@@ -4,85 +4,84 @@ import pprint
 
 def connect(ikey, skey, host):
   try:
-    print("############## CONECTANDO NO DUO ###############")
+    print(" CONECTANDO NO DUO ".center(50, '#'))
     admin_api = duo_client.Admin(ikey=ikey, skey=skey , host=host)
     return admin_api
   except error:
-    print("############# ERROR NA CONEXAO")
+    print(" ERRO NA CONEXAO ".center(50, '#'))
     print(error)
     return False
 
 def create_user(admin_api,username, realname):
   try:
-    print("############## CRIANDO USUARIO ###############")  
+    print(" CRIANDO USUARIO ".center(50, '#'))  
     user = admin_api.add_user(
         username=username, 
         realname=realname,        
-        alias1=f'itau\{username}',
+        alias1=username,
         status = 'active'
         )
-    #pprint.pprint(user)
+
     return user
   except RuntimeError as e:
-    print("############# ERROR NA CRIACAO DO USUARIO")  
-    print(e)
-    return False
+    print(" ERRO NA CRIACAO DO USUARIO ".center(50, '#'))  
+    raise
 
 def create_phone(admin_api, number):
   try:
-    print("############## CRIANDO PHONE ###############")    
+    print(" CRIANDO PHONE ".center(50, '#'))    
     phone = admin_api.add_phone(number=number,type='mobile',platform='generic smartphone')
-    #pprint.pprint(phone)
     return phone
   except RuntimeError as e:
-    print("############# ERROR NA CRIACAO DO PHONE #############")    
-    print(e)  
-
+    print(" ERRO NA CRIACAO DO PHONE ".center(50, '#'))    
+    raise  
+     
 def add_user_phone(admin_api, user, phone):
   try:
-    print("############## ADD USER NO PHONE ##########")
+    print(" ADD USER NO PHONE ".center(50, '#'))
     admin_api.add_user_phone(user_id=user['user_id'],phone_id=phone['phone_id']) 
     return True
   except RuntimeError as e:
-    print("############# ERROR ADD USER NO PHONE #############")      
-    print(e)
-    return False
+    print(" ERRO ADD USER NO PHONE ".center(50, '#'))      
+    raise
 
 def add_user_group(admin_api,user, group_id):
     try:
-      print("############# ADICINANDO O USUARIO NO GROUP ################")
+      print(" ADICINANDO O USUARIO NO GROUP ".center(50, '#'))
       admin_api.add_user_group(user['user_id'], group_id)
       return True
     except RuntimeError as e:
-      print("############# ERRO AO ADICIONAR USUARIO NO GROUP #############")        
-      print(e)
-      return False
-      
+      print(" ERRO AO ADICIONAR USUARIO NO GROUP ".center(50, '#'))        
+      raise      
     
-
 def send_sms(admin_api, phone):
   try:
-    print("############# SEND SMS ################")      
+    print(" SEND SMS ".center(50, '#'))      
     act_sent = admin_api.send_sms_activation_to_phone(phone_id=phone['phone_id'],install='1')
     print('SMS activation sent to', phone['number'] + ':')
     return True
   except RuntimeError as e:
-    print("############# ERRO NO SEND SMS #############")        
-    print(e)
-    return False
+    print(" ERRO NO SEND SMS ".center(50, '#'))        
+    raise
 
 
 ikey=""
 skey=""
-host=""
+host="
 GROUP_ID = ''
+
+
+lista = [
+          ['clodonil','Clodonil Honorio Trigo', '+55-11-xxxxxxx']
+        ]
 
 
 for users in lista:
     username = users[0].lower()
     nome = users[1].title()
     phone = users[2]
-    print("########## CRIANDO MFA: {0}".format(nome))
+    banner = " CRIANDO MFA: {0} ".format(nome)
+    print(banner.center(50,"#"))
     conn = connect(ikey, skey, host)
     try:
       if conn:
@@ -94,8 +93,6 @@ for users in lista:
                if add_user_phone(conn,user,obj_phone):
                   send_sms(conn,obj_phone) 
     except RuntimeError as e:
-      print("############# ERRO NO MFA #############")        
+      print(" ERRO NO MFA ".center(50,'#'))        
       print(e)
-        
    
-
